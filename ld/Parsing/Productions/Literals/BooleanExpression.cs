eaf2447be9050bf0e6c4d7e6cd9ac963e8024128
deeton rushy <deetonrushton1@outlook.com>
2023-09-03 09:10:29 +0100

    BooleanExpr: Add `true` and `false`.

---
+++ ld/Parsing/Productions/Literals/BooleanExpression.cs
@@ -0,0 +1,21 @@
+﻿
+using Language.Lexing;
+using Spectre.Console;
+
+namespace Language.Parsing.Productions.Literals;
+
+public record class BooleanExpression(bool Value, SourceLocation Location)
+    : Expression(Location)
+{
+    public override object Visit(IAstVisitor visitor)
+    {
+        return visitor.VisitBooleanExpression(this);
+    }
+
+    public override void Visualize(string indent, bool last)
+    {
+        _ = ShowIndent(indent, last);
+        string repr = Value ? "true" : "false";
+        AnsiConsole.WriteLine(repr);
+    }
+}
