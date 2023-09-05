4d998ec53bd8bb3f3856dd3dde9978681fedaf7d
deeton rushy <deetonrushton1@outlook.com>
2023-09-05 18:32:26 +0100

    Project: this will contain information about the project created with the command line toolchain.

---
+++ ld/ProjectDetails.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Language;
+
+public class ProjectDetails
+{
+    public string RootDir { get; set; }
+    public string DependenciesDir { get; set; }
+
+    public ProjectDetails(string rootDir, string dependenciesDir)
+    {
+        RootDir = rootDir;
+        DependenciesDir = dependenciesDir;
+    }
+}
