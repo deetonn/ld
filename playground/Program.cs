d05a52d42463bd02ed17abbae9d976020f2409e2
deeton rushy <deetonrushton1@outlook.com>
2023-09-05 18:32:39 +0100

    Test Project: just tinkering and testing

--- playground/Program.cs
+++ playground/Program.cs
@@ -1,5 +1,7 @@
-﻿using Language.Lexing;
+﻿using Language;
+using Language.Lexing;
 using Language.Parsing;
+using Language.TypeChecking;
 using Spectre.Console;
 using System.Runtime.InteropServices;
 
@@ -10,16 +12,22 @@ var path = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
 var fileInfo = new FileInfo(path);
 var lexer = new Lexer(fileInfo);
 
-var tokens = TimeExpression("lexer", lexer.LexTokens);
+var projectDetails = new ProjectDetails($"C:\\Users\\{Environment.UserName}\\source\\repos\\ld\\playground",
+    $"C:\\Users\\{Environment.UserName}\\source\\repos\\ld\\playground\\deps");
+
+var tokens = TimedExpression("[yellow]lexer[/]", lexer.LexTokens);
 var fileContents = File.ReadAllText(fileInfo.FullName);
-var ast = TimeExpression("parser", () => new Parser(tokens, fileContents).ParseTokens());
+var ast = TimedExpression("[cyan]parser[/]", () => new Parser(fileInfo.Name, projectDetails, tokens, fileContents).ParseTokens());
 
 foreach (var node in ast)
 {
     node.Visualize(string.Empty, false);
 }
 
-static T TimeExpression<T>(string name, Func<T> f)
+var checker = TimedExpression("[red]typechecker[/]", () => new TypeChecker(ast.ToList(), fileContents));
+checker.Check();
+
+static T TimedExpression<T>(string name, Func<T> f)
 {
     var start = DateTime.Now;
     var expr = f();
