06bab097b43215ba117e757a2cdc88eb549424e6
deeton rushy <deetonrushton1@outlook.com>
2023-09-05 18:22:40 +0100

    Optimizer: begin work on an optmizer
    This just exists as a shell for now.

---
+++ ld/Optimization/Optimizer.cs
@@ -0,0 +1,31 @@
+﻿
+using Language.Parsing.Productions;
+
+namespace Language.Optimization;
+
+public class Optimizer
+{
+    private List<AstNode> OptimizedAst;
+    private List<AstNode> OriginalAst;
+
+    public Optimizer(List<AstNode> ast)
+    {
+        OriginalAst = ast;
+        OptimizedAst = new List<AstNode>();
+    }
+
+    public List<AstNode> Optimize()
+    {
+        foreach (var node in OriginalAst)
+        {
+            OptimizeNode(node);
+        }
+
+        return OptimizedAst;
+    }
+
+    private void OptimizeNode(AstNode node)
+    {
+
+    }
+}
