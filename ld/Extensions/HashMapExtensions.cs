34dc0dd7151ca0a2d6c99d6fca254ed4b992eadb
deeton rushy <deetonrushton1@outlook.com>
2023-09-05 18:20:17 +0100

    Extensions: add hashmap extensions
    The parser uses HashMaps instead now because we don't want duplicates.

---
+++ ld/Extensions/HashMapExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Language.Parsing.Productions;
+
+namespace Language.Extensions;
+
+public static class HashMapExtensions
+{
+    public static void AddRange(this HashSet<AstNode> set, HashSet<AstNode> other)
+    {
+        foreach (var thing in other)
+            set.Add(thing);
+    }
+
+    public static void AddRange(this HashSet<AstNode> set, IList<AstNode> other)
+    {
+        foreach (var thing in other)
+            set.Add(thing);
+    }
+}
