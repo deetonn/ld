610b89999fba7300b69b66b8ee4b1ac972b49c65
deeton rushy <deetonrushton1@outlook.com>
2023-09-18 12:23:10 +0100

    Logger: add global logging system

---
+++ ld/ErrorHandling/Logger.cs
@@ -0,0 +1,32 @@
+﻿
+using Language.Config;
+
+namespace Language.ErrorHandling;
+
+internal static class Logger
+{
+    private static readonly string _fileName;
+
+    static Logger()
+    {
+        _fileName = "compiler.log";
+
+        if (File.Exists(_fileName))
+            File.Delete(_fileName);
+
+        using var file = File.Create(_fileName);
+    }
+
+    private static void LogBase(object self, string message)
+    {
+        File.AppendAllText(_fileName, $"{self.GetType().Name}: {message}");
+    }
+
+    public static void LogDebug(object self, string message)
+    {
+        if (CompilerConfig.GetFlag("verbose"))
+        {
+            LogBase(self, $"debug: {message}");
+        }
+    }
+}
